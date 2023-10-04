6a6c7a803c704b639418724e47933d2dfe81ecf8
LofiLeon <43483382+LofiLeon@users.noreply.github.com>
2023-10-04 18:20:37 +0200

    Initial Commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Leon Grimmeisen on 04.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
