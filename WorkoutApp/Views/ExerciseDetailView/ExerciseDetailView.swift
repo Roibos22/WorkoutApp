7bb96a7c7894d3c0807790d17496811b3fadf0fe
LofiLeon <43483382+LofiLeon@users.noreply.github.com>
2023-10-11 10:49:07 +0200

    Added Initial ExerciseDetail View

---
+++ WorkoutApp/Views/ExerciseDetailView/ExerciseDetailView.swift
@@ -0,0 +1,116 @@
+//
+//  ExerciseDetailView.swift
+//  WorkoutApp
+//
+//  Created by Leon Grimmeisen on 11.10.23.
+//
+
+import SwiftUI
+
+struct ExerciseSettingsRemoveCard: View {
+    
+    let exercise: Exercise
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Text("")
+                .offset(x: 15, y: -43)
+                .foregroundColor(.gray)
+                .bold()
+                .font(.subheadline)
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(Color.red)
+                .frame(height: 60)
+                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2))
+            HStack {
+                Spacer()
+                Text("Remove Exercise")
+                Spacer()
+            }
+            .padding()
+            .font(.title2)
+            .bold()
+        }
+        .padding(.vertical, 10)
+
+    }
+}
+
+struct ExerciseSettingsTitleCard: View {
+    
+    let exercise: Exercise
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Text("Exercise Title")
+                .offset(x: 15, y: -43)
+                .foregroundColor(.gray)
+                .bold()
+                .font(.subheadline)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.black, lineWidth: 2)
+                .frame(height: 60)
+                .foregroundColor(.white)
+            HStack {
+                Text("Exercise Title")
+            }
+            .padding()
+            .font(.title)
+            .bold()
+        }
+        .padding(.vertical, 10)
+    }
+}
+
+struct ExerciseSettingsValueCard: View {
+    
+    let exercise: Exercise
+    let title: String
+    let icon: String
+    let value: Double
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Text(title)
+                .offset(x: 15, y: -43)
+                .foregroundColor(.gray)
+                .bold()
+                .font(.subheadline)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.black, lineWidth: 2)
+                .frame(height: 60)
+                .foregroundColor(.white)
+            HStack {
+                Image(systemName: icon)
+                Spacer()
+                Text(value.asDigitalMinutes())
+            }
+            .padding()
+            .font(.title)
+            .bold()
+        }
+        .padding(.vertical, 10)
+    }
+}
+
+struct ExerciseDetailView: View {
+    
+    let exercise: Exercise
+    
+    var body: some View {
+        VStack {
+            ExerciseSettingsTitleCard(exercise: exercise)
+            ExerciseSettingsValueCard(exercise: exercise, title: "Exercise Duration", icon: "stopwatch", value: exercise.duration)
+            ExerciseSettingsValueCard(exercise: exercise, title: "Exercise Rest", icon: "hourglass", value: exercise.rest)
+            ExerciseSettingsRemoveCard(exercise: exercise)
+        }
+        .padding(.horizontal)
+        .navigationTitle(exercise.title)
+    }
+}
+
+struct ExerciseDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseDetailView(exercise: Workout.sampleWorkouts[0].exercises[0])
+    }
+}
--- WorkoutApp/Views/WorkoutDetailView/WorkoutDetailView.swift
+++ WorkoutApp/Views/WorkoutDetailView/WorkoutDetailView.swift
@@ -7,139 +7,12 @@
 
 import SwiftUI
 
-struct WorkoutDetailViewButtonSection: View {
-    
-    let workout: Workout
-    
-    var body: some View {
-        HStack {
-            // Preview
-            NavigationLink {
-                // open Preview
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(height: 60)
-                        .foregroundColor(Color.blue)
-                    HStack {
-                        Image(systemName: "clock.fill")
-                        Text(workout.duration.asDigitalMinutes())
-                    }
-                    .font(.title3)
-                    .bold()
-                    .foregroundColor(.white)
-                }
-            }
-            // Completions and History
-            NavigationLink {
-                // open History
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(height: 60)
-                        .foregroundColor(Color.blue)
-                    HStack {
-                        Image(systemName: "clock.arrow.circlepath")
-                        Text("\(workout.completions)x")
-                    }
-                    .font(.title3)
-                    .bold()
-                    .foregroundColor(.white)
-                }
-            }
-            // Start Workout
-            NavigationLink {
-                // Workout
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(height: 60)
-                        .foregroundColor(Color.blue)
-                    Text("GO!")
-                    .font(.title3)
-                    .bold()
-                    .foregroundColor(.white)
-                }
-            }
-        }
-    }
-}
-
-struct ExerciseCardView: View {
-    
-    var exercise: Exercise
-    
-    var body: some View {
-        ZStack(alignment: .top) {
-            
-            // Frame of Rectangle
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(Color.black, lineWidth: 2)
-                .frame(height: 100)
-                .foregroundColor(.white)
-
-            // Content of Rectangle
-            VStack(alignment: .leading) {
-                
-                // Rectangle Title
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(height: 50)
-                        .foregroundColor(Color.blue)
-                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 2))
-                    Text(exercise.title)
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                
-                // Exercise Information
-                HStack(alignment: .center) {
-                    Spacer()
-                    HStack(alignment: .center) {
-                        Image(systemName: "stopwatch")
-                        Text(exercise.rest.asDigitalMinutes())
-                    }
-                    Spacer()
-                    HStack(alignment: .center) {
-                        Image(systemName: "hourglass")
-                        Text(exercise.duration.asDigitalMinutes())
-                    }
-                    Spacer()
-                }
-                .font(.title2)
-                .bold()
-            }
-        }
-    }
-}
-
-struct WorkoutDetailViewExercisesSection: View {
-    
-    let workout: Workout
-    
-    var body: some View {
-        VStack {
-            HStack() {
-                Image(systemName: "dumbbell.fill")
-                Text("Exercises")
-                Spacer()
-            }
-            .font(.title2)
-            .bold()
-            ForEach(workout.exercises) { exercise in
-                ExerciseCardView(exercise: exercise)
-            }
-        }
-    }
-}
-
 struct WorkoutDetailView: View {
     
     let workout: Workout
     
     var body: some View {
-        NavigationView {
+
             ScrollView {
                 VStack {
                     
@@ -158,7 +31,7 @@ struct WorkoutDetailView: View {
                 .navigationTitle(workout.title)
             }
         }
-    }
+
 }
 
 struct WorkoutDetailView_Previews: PreviewProvider {
