ec1d684909df30322a5b47e0ddc6aab3a9fee3a2
LofiLeon <grimmeisen.leon@gmail.com>
2024-08-03 01:27:51 +0200

    refactor: add FileManager Extension

--- WorkoutAppOld/DataManager/FileManager.swift
+++ WorkoutAppOld/DataManager/FileManager.swift
@@ -7,9 +7,4 @@
 
 import Foundation
 
-extension FileManager {
-    static var documentsDirectory: URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-}
+
