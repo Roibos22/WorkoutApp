12e1603170d9e4bc71eb01333e61924b45a715b4
LofiLeon <grimmeisen.leon@gmail.com>
2024-09-01 00:44:43 +0200

    refactor: refactor LegalNotice View

--- WorkoutApp/Views/SettingsView.swift
+++ WorkoutApp/Views/SettingsView.swift
@@ -143,28 +143,48 @@ struct LegalNoticeView: View {
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-                vStack(title: "Legal Notice", content: """
-                Angaben gemäß § 5 TMG
+            VStack(alignment: .leading, spacing: 15) {
+                VStack(alignment: .leading) {
+                    Text("Legal Notice")
+                        .font(.title2)
+                        .bold()
+                    Text(String("""
+                    Angaben gemäß § 5 TMG
+                    
+                    Leon Grimmeisen
+                    Petersburger Straße 42
+                    10249 Berlin
+                    """))
+                }
                 
-                Leon Grimmeisen
-                Petersburger Straße 42
-                10249 Berlin
-                """)
+                VStack(alignment: .leading) {
+                    Text("Kontakt")
+                        .font(.title2)
+                        .bold()
+                    Text(String("""
+                    Telefon: +491743629023
+                    E-Mail: lmgrimmeisen(at)gmail.com
+                    """))
+                }
                 
-                vStack(title: "Kontakt", content: """
-                Telefon: +491743629023
-                E-Mail: lmgrimmeisen(at)gmail.com
-                """)
+                VStack(alignment: .leading) {
+                    Text("Haftung für Links")
+                        .font(.title2)
+                        .bold()
+                    Text(String("""
+                    Diese App enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung, soweit möglich, auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+                    """))
+                }
                 
-                vStack(title: "Haftung für Links", content: """
-                Diese App enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung, soweit möglich, auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-                """)
-                
-                vStack(title: "Quelle", content: """
-                Link: e-recht24
-                Destination: https://www.e-recht24.de/impressum-generator.html
-                """)
+                VStack(alignment: .leading) {
+                    Text("Quelle")
+                        .font(.title2)
+                        .bold()
+                    Text(String("""
+                    Link: e-recht24
+                    Destination: https://www.e-recht24.de/impressum-generator.html
+                    """))
+                }
             }
             .navigationTitle("Legal Notice")
             .frame(maxWidth: .infinity, alignment: .leading)
@@ -173,13 +193,12 @@ struct LegalNoticeView: View {
     }
     
     private func vStack(title: LocalizedStringResource, content: String) -> some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 15) {
             Text(title)
                 .font(.title2)
                 .bold()
             Text(content)
         }
-        .padding(.vertical, 15)
     }
 }
 
