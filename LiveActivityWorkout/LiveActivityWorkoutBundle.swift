838b08910bcca28a88869b11cc0c1bb390007e42
LofiLeon <grimmeisen.leon@gmail.com>
2024-09-04 18:17:29 +0200

    feat: add live initial live activity target

---
+++ LiveActivityWorkout/LiveActivityWorkoutBundle.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityWorkoutBundle.swift
+//  LiveActivityWorkout
+//
+//  Created by Leon Grimmeisen on 04.09.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiveActivityWorkoutBundle: WidgetBundle {
+    var body: some Widget {
+        LiveActivityWorkout()
+        LiveActivityWorkoutLiveActivity()
+    }
+}
